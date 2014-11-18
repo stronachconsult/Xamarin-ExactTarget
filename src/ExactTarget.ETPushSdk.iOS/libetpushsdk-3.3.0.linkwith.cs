6a3aa6cc8686eddfd0a0cfb3df1f392ffc908d47
Sky Morey <smorey@degdigital.com>
2014-11-18 10:42:59 -0600

    modified linkwith.cs

--- src/ExactTarget.ETPushSdk.iOS/libetpushsdk-3.3.0.linkwith.cs
+++ src/ExactTarget.ETPushSdk.iOS/libetpushsdk-3.3.0.linkwith.cs
@@ -1,5 +1,5 @@
 using System;
 using MonoTouch.ObjCRuntime;
 
-[assembly: LinkWith("libetpushsdk-3.3.0.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "MobileCoreServices Security CoreLocation CoreTelephony SystemConfiguration")]
+[assembly: LinkWith("libetpushsdk-3.3.0.a", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true, Frameworks = "CoreLocation")]
 [assembly: LinkWith("libsqlite3.dylib", LinkTarget.ArmV7 | LinkTarget.ArmV7s | LinkTarget.Simulator, ForceLoad = true)]
